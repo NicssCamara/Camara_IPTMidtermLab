3483a1ab1566eaa471103be1594af86d7ceb30ef
Nicole Camara <vandemonnicoo@gmail.com>
2025-04-03 10:31:40 +0800

    ipt-lab-updated

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Retrieve all products.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products, Response::HTTP_OK);
+    }
+
+    /**
+     * Validate and store a new product.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0',
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json($product, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Retrieve a single product by ID.
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json($product, Response::HTTP_OK);
+    }
+
+    /**
+     * Update a product.
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|required|numeric|min:0',
+            'quantity' => 'sometimes|required|integer|min:0',
+        ]);
+
+        $product->update($request->all());
+
+        return response()->json($product, Response::HTTP_OK);
+    }
+
+    /**
+     * Delete a product by ID.
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted successfully'], Response::HTTP_OK);
+    }
+}
